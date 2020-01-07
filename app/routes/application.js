404a1e7c2a2b211500174dd738025eb97b495936
James Herdman <james.herdman@gmail.com>
2020-01-07 09:30:53 -0500

    Bugs

---
+++ app/routes/application.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+
+import { inject as service } from '@ember/service';
+
+export default class ApplicationRoute extends Route {
+  @service router;
+
+  activate() {
+    super.activate(...arguments);
+
+    console.log('here you are:', this.router.currentURL);
+  }
+}
