29823e4d9a209e817d3c44cab5274f3bbaa28016
James Herdman <james.herdman@gmail.com>
2020-01-07 09:35:22 -0500

    More Logging

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,12 +2,31 @@ import Route from '@ember/routing/route';
 
 import { inject as service } from '@ember/service';
 
+import { action } from '@ember/object';
+
 export default class ApplicationRoute extends Route {
   @service router;
+
+  beforeModel() {
+    console.log('BEFORE MODEL: here you are:', this.router.currentURL);
+  }
+
+  model() {
+    console.log('MODEL: here you are:', this.router.currentURL);
+  }
+
+  afterModel() {
+    console.log('AFTER MODEL: here you are:', this.router.currentURL);
+  }
 
   activate() {
     super.activate(...arguments);
 
-    console.log('here you are:', this.router.currentURL);
+    console.log('ACTIVATE: here you are:', this.router.currentURL);
+  }
+
+  @action
+  didTransition() {
+    console.log('DID TRANSITION: here you are:', this.router.currentURL);
   }
 }
